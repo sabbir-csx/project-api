99c00eddf0672f1fd2e50a8543084776abe8e015
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-01-04 17:12:09 +0600

    React components added

--- front-end/project-api-ui/src/App.js
+++ front-end/project-api-ui/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./Home";
+import { Department } from "./Department";
+import { Employee } from "./Employee";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">ReactJS Front-End</h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-ourline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-ourline-primary"
+                to="/department"
+              >
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-ourline-primary"
+                to="/employee"
+              >
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <switch>
+          <Route path="/home" component={Home} />
+          <Route path="/department" component={Department} />
+          <Route path="/employee" component={Employee} />
+        </switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
