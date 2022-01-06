fa062086f068dc4a99b9520325bb1b107c2d06ff
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-01-06 17:32:24 +0600

    departments-data-show-success

--- front-end/project-api-ui/src/App.js
+++ front-end/project-api-ui/src/App.js
@@ -1,4 +1,4 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 import { Home } from "./Home";
 import { Department } from "./Department";
@@ -9,26 +9,26 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App container">
-        <h3 className="d-flex justify-content-center m-3">ReactJS Front-End</h3>
+        <h3 className="d-flex justify-content-center m-4">React JS Frontend</h3>
 
         <nav className="navbar navbar-expand-sm bg-light navbar-dark">
           <ul className="navbar-nav">
-            <li className="nav-item m-1">
-              <NavLink className="btn btn-light btn-ourline-primary" to="/home">
+            <li className="nav-item- m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
                 Home
               </NavLink>
             </li>
-            <li className="nav-item m-1">
+            <li className="nav-item- m-1">
               <NavLink
-                className="btn btn-light btn-ourline-primary"
+                className="btn btn-light btn-outline-primary"
                 to="/department"
               >
                 Department
               </NavLink>
             </li>
-            <li className="nav-item m-1">
+            <li className="nav-item- m-1">
               <NavLink
-                className="btn btn-light btn-ourline-primary"
+                className="btn btn-light btn-outline-primary"
                 to="/employee"
               >
                 Employee
@@ -36,11 +36,12 @@ function App() {
             </li>
           </ul>
         </nav>
-        <switch>
+
+        <Switch>
           <Route path="/home" component={Home} />
           <Route path="/department" component={Department} />
           <Route path="/employee" component={Employee} />
-        </switch>
+        </Switch>
       </div>
     </BrowserRouter>
   );
